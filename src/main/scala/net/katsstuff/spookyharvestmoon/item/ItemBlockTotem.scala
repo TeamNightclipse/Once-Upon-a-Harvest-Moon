bda2d03296919c1c49e20904778ebd815917e6f5
ArekkuusuJerii <edgargcx99@gmail.com>
2017-10-07 18:22:40 -0500

    Fix Totem transforms, add model override for the broken totem property

--- src/main/scala/net/katsstuff/spookyharvestmoon/item/ItemBlockTotem.scala
+++ src/main/scala/net/katsstuff/spookyharvestmoon/item/ItemBlockTotem.scala
@@ -2,9 +2,15 @@ package net.katsstuff.spookyharvestmoon.item
 
 import net.katsstuff.spookyharvestmoon.helper.ItemNBTHelper
 import net.minecraft.block.Block
+import net.minecraft.entity.EntityLivingBase
 import net.minecraft.item.{ItemBlock, ItemStack}
+import net.minecraft.util.ResourceLocation
+import net.minecraft.world.World
 
 class ItemBlockTotem(block: Block) extends ItemBlock(block) {
+  addPropertyOverride(new ResourceLocation("broken"), (stack: ItemStack, _: World, _: EntityLivingBase) => {
+    if (ItemNBTHelper.getBoolean(stack, "Broken")) 1F else 0F
+  })
 
   override def getUnlocalizedName(stack: ItemStack): String = {
     if(ItemNBTHelper.getBoolean(stack, "Broken")) {
